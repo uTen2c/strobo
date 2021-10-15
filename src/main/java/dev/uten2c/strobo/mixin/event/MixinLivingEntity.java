e6cbbd1d4a6d72234eaa9e2e506df66d8732ce3e
uTen2c <uten2c@gmail.com>
2021-10-16 08:47:52 +0900

    Damage系イベントでダメージ量を変更できるようにした

--- src/main/java/dev/uten2c/strobo/mixin/event/MixinLivingEntity.java
+++ src/main/java/dev/uten2c/strobo/mixin/event/MixinLivingEntity.java
@@ -4,31 +4,49 @@ import dev.uten2c.strobo.event.entity.EntityDamageByEntityEvent;
 import dev.uten2c.strobo.event.entity.EntityDamageEvent;
 import dev.uten2c.strobo.event.entity.EntityDeathEvent;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
-public class MixinLivingEntity {
+public abstract class MixinLivingEntity extends Entity {
+
+    private float strobo$eventDamageAmount;
+
+    public MixinLivingEntity(EntityType<?> type, World world) {
+        super(type, world);
+    }
 
     @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
     private void callEntityDamageEvent(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity entity = ((LivingEntity) (Object) this);
-        EntityDamageEvent damageEvent = new EntityDamageEvent(entity, amount, source);
+        var finalAmount = amount;
+        EntityDamageEvent damageEvent = new EntityDamageEvent(entity, finalAmount, source);
         if (!damageEvent.callEvent()) {
             cir.setReturnValue(false);
         }
+        finalAmount = damageEvent.getAmount();
         Entity attacker = source.getAttacker();
         if (attacker != null) {
-            EntityDamageByEntityEvent damageByEntityEvent = new EntityDamageByEntityEvent(entity, amount, source, attacker);
+            EntityDamageByEntityEvent damageByEntityEvent = new EntityDamageByEntityEvent(entity, finalAmount, source, attacker);
             if (!damageByEntityEvent.callEvent()) {
                 cir.setReturnValue(false);
             }
+            finalAmount = damageByEntityEvent.getAmount();
         }
+        strobo$eventDamageAmount = finalAmount;
+    }
+
+    @ModifyVariable(method = "damage", at = @At("HEAD"), ordinal = 0)
+    private float callEntityDamageEvent(float amount) {
+        return strobo$eventDamageAmount;
     }
 
     @Inject(method = "onDeath", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;setPose(Lnet/minecraft/entity/EntityPose;)V", shift = At.Shift.AFTER))
