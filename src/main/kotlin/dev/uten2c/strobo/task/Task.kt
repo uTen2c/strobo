56298d76a6a119a6af8c03f2f65e55056771af45
uTen2c <uten2c@gmail.com>
2021-10-20 08:10:03 +0900

    runSyncメソッドを追加

--- src/main/kotlin/dev/uten2c/strobo/task/Task.kt
+++ src/main/kotlin/dev/uten2c/strobo/task/Task.kt
@@ -4,6 +4,10 @@ fun nextTick(block: () -> Unit) {
     TaskRunner.tasks.add(TaskRunner.Task(1, block))
 }
 
+fun runSync(block: () -> Unit) {
+    TaskRunner.tasks.add(TaskRunner.Task(0, block))
+}
+
 fun waitAndRun(ticks: Long, block: () -> Unit) {
     TaskRunner.tasks.add(TaskRunner.Task(ticks, block))
 }
