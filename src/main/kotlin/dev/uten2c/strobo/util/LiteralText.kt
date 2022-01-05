63e49b6ce09919ece7d46b800129cb9fadfe05ca
uTen2c <uten2c@gmail.com>
2022-01-06 08:41:45 +0900

    textの引数にCharを渡せるようにした

--- src/main/kotlin/dev/uten2c/strobo/util/LiteralText.kt
+++ src/main/kotlin/dev/uten2c/strobo/util/LiteralText.kt
@@ -8,6 +8,11 @@ import net.minecraft.text.LiteralText
 fun text(string: String) = LiteralText(string)
 
 /**
+ * [LiteralText]のコンストラクタのエイリアス
+ */
+fun text(char: Char) = LiteralText(char.toString())
+
+/**
  * 空文字列の[LiteralText]を生成
  */
 fun emptyText(): LiteralText = LiteralText("")
