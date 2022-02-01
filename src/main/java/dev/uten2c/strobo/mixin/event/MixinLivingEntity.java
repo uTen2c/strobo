1ee939952d6b4da960d62fcf1dc29a154306db47
uTen2c <uten2c@gmail.com>
2022-02-02 01:30:11 +0900

    バグ修正

--- src/main/java/dev/uten2c/strobo/mixin/event/MixinLivingEntity.java
+++ src/main/java/dev/uten2c/strobo/mixin/event/MixinLivingEntity.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -70,8 +70,8 @@ public abstract class MixinLivingEntity extends Entity {
         strobo$eventDamageAmount = event.getAmount();
     }
 
-    @ModifyVariable(method = "damage", at = @At("HEAD"), ordinal = 0, argsOnly = true)
-    private float callEntityDamageEvent(float amount) {
+    @ModifyArg(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;applyDamage(Lnet/minecraft/entity/damage/DamageSource;F)V"), index = 1)
+    private float applyEventDamage(float amount) {
         return strobo$eventDamageAmount;
     }
 
