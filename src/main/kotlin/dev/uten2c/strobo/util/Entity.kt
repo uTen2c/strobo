c890d635ba7040bccebe5c2da86b9cbd8b177f50
uTen2c <uten2c@gmail.com>
2022-09-01 14:15:37 +0900

    Use getRotationVec

--- src/main/kotlin/dev/uten2c/strobo/util/Entity.kt
+++ src/main/kotlin/dev/uten2c/strobo/util/Entity.kt
@@ -14,12 +14,7 @@ import net.minecraft.util.math.Vec3d
  * エンティティの見ている向きのベクトルを取得
  */
 val Entity.direction: Vec3d
-    get() {
-        val x = -MathHelper.sin(yaw * 0.017453292f) * MathHelper.cos(pitch * 0.017453292f)
-        val y = -MathHelper.sin(pitch * 0.017453292f)
-        val z = MathHelper.cos(yaw * 0.017453292f) * MathHelper.cos(pitch * 0.017453292f)
-        return Vec3d(x.toDouble(), y.toDouble(), z.toDouble()).normalize()
-    }
+    get() = getRotationVec(1f)
 
 /**
  * エンティティの座標系を取得
